73481698fd1cc7dbce8e8b14721797fc9da3d17c
Darpan Deva <flp9001@gmail.com>
2019-09-04 12:09:42 -0300

    Upload GUI Validation
    -Now the upload dialog validates the fields before allowing user to try
    to upload.
    
    -Also the Category was added

--- gui/wilber_gui_upload.py
+++ gui/wilber_gui_upload.py
@@ -1,12 +1,15 @@
 # coding: utf-8
 from __future__ import unicode_literals, print_function, division
-
+from wilber_common import ASSET_TYPE_TO_CATEGORY
 import os
-
+import logging
 import gtk
 
 
 DEBUG = False
+
+logger = logging.getLogger('wilber.gui.upload')
+
 
 class Folder(object):
     def __init__(self, folder):
@@ -15,6 +18,7 @@ class Folder(object):
     def __enter__(self):
         self.previous_dir = os.getcwd()
         os.chdir(self.folder)
+        return self.folder
 
     def __exit__(self, exc_type, exc_value, tb):
         os.chdir(self.previous_dir)
@@ -22,8 +26,6 @@ class Folder(object):
 
 class WilberUploadDialog(object):
     def __init__(self, folder='.'):
-
-
         self.dialog = gtk.Dialog(
             "Wilber Upload",
             None,
@@ -32,30 +34,31 @@ class WilberUploadDialog(object):
              gtk.STOCK_OK, gtk.RESPONSE_ACCEPT)
         )
 
-        self.create_widgets()
-
         self.image_filename = None
+        self.base_folder = folder
 
+        self.create_widgets()
         self.connect_signals()
         self.dialog.connect("destroy", self.destroy)
         self.dialog.show_all()
-        self.base_folder = folder
+
+        self.validate_data()
 
     def create_widgets(self):
-
-
-        label_1 = gtk.Label("Name:")
-        label_2 = gtk.Label("Description:")
+        label_name = gtk.Label("Name:")
+        label_description = gtk.Label("Description:")
+        label_category = gtk.Label("Category:")
         self.button_select_image = gtk.Button("Select Thumbnail")
         self.button_add_file = gtk.Button("Add File")
         self.button_remove_file = gtk.Button("Remove Selected")
         self.entry_name = gtk.Entry()
         self.entry_description = gtk.TextView()
         self.entry_description.set_size_request(200, 100)
+        self.category_dropdown = gtk.combo_box_new_text()
+        for asset_type in ASSET_TYPE_TO_CATEGORY.keys():
+            self.category_dropdown.append_text(asset_type)
 
         self.image_widget = gtk.Image()
-
-
         self.files_model = gtk.ListStore(str)
 
         scrollable_view = gtk.ScrolledWindow()
@@ -70,33 +73,38 @@ class WilberUploadDialog(object):
 
         table = gtk.Table(rows=2, columns=2)
         self.dialog.vbox.pack_start(table)
-        table.attach(label_1, 0, 1, 0, 1, False, False, 5, 5)
-        table.attach(label_2, 0, 1, 1, 2, False, False, 5, 5)
-        table.attach(self.button_select_image, 0, 1, 2, 3, False, False, 5, 5)
-        table.attach(self.button_add_file, 0, 1, 3, 4, False, False, 5, 5)
-        table.attach(self.button_remove_file, 1, 2, 4, 5, False, False, 5, 5)
 
-        table.attach(self.entry_name,             1, 2, 0, 1, gtk.EXPAND|gtk.FILL, False, 5, 5)
-        table.attach(self.entry_description,             1, 2, 1, 2, gtk.EXPAND|gtk.FILL, gtk.EXPAND|gtk.FILL, 5 ,5)
-        table.attach(self.image_widget,        1, 2, 2, 3, gtk.EXPAND|gtk.FILL, gtk.FILL, 5 ,5)
-        table.attach(scrollable_view,         1, 2, 3, 4, gtk.EXPAND|gtk.FILL, gtk.EXPAND|gtk.FILL, 5 ,5)
+        table.attach(label_name,                0, 1, 0, 1, False, False, 5, 5)
+        table.attach(label_description,         0, 1, 1, 2, False, False, 5, 5)
+        table.attach(self.button_select_image,  0, 1, 2, 3, False, False, 5, 5)
+        table.attach(label_category,            0, 1, 3, 4, False, False, 5, 5)
+        table.attach(self.button_add_file,      0, 1, 4, 5, False, False, 5, 5)
+        table.attach(self.button_remove_file,   0, 1, 5, 6, False, False, 5, 5)
 
+        table.attach(self.entry_name,           1, 2, 0, 1, gtk.EXPAND | gtk.FILL, False, 5, 5)
+        table.attach(self.entry_description,    1, 2, 1, 2, gtk.EXPAND | gtk.FILL, gtk.EXPAND | gtk.FILL, 5, 5)
+        table.attach(self.image_widget,         1, 2, 2, 3, gtk.EXPAND | gtk.FILL, gtk.FILL, 5, 5)
+        table.attach(self.category_dropdown,    1, 2, 3, 4, gtk.EXPAND | gtk.FILL, False, 5, 5)
+        table.attach(scrollable_view,           1, 2, 4, 6, gtk.EXPAND | gtk.FILL, gtk.EXPAND | gtk.FILL, 5, 5)
 
     def connect_signals(self):
         self.button_select_image.connect("clicked", self.select_image)
         self.button_add_file.connect("clicked", self.add_file)
         self.button_remove_file.connect("clicked", self.remove_file)
 
+        self.entry_name.connect("focus-out-event", self.validate_data)
+        self.entry_description.connect("focus-out-event", self.validate_data)
+        self.category_dropdown.connect("changed", self.validate_data)
 
     def select_image(self, widget):
-        dialog=gtk.FileChooserDialog(title="Select Image", action=gtk.FILE_CHOOSER_ACTION_OPEN, buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+        dialog = gtk.FileChooserDialog(title="Select Image", action=gtk.FILE_CHOOSER_ACTION_OPEN, buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK))
 
-        filter = gtk.FileFilter()
-        filter.set_name("Images")
-        filter.add_mime_type("image/jpeg")
-        filter.add_mime_type("image/png")
-        filter.add_mime_type("image/gif")
-        dialog.add_filter(filter)
+        file_filter = gtk.FileFilter()
+        file_filter.set_name("Images")
+        file_filter.add_mime_type("image/jpeg")
+        file_filter.add_mime_type("image/png")
+        file_filter.add_mime_type("image/gif")
+        dialog.add_filter(file_filter)
 
         response = dialog.run()
         filename = None
@@ -105,13 +113,12 @@ class WilberUploadDialog(object):
             pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(self.image_filename, 200, 200)
             self.image_widget.set_from_pixbuf(pixbuf)
 
-
         dialog.destroy()
+        self.validate_data()
         return filename
 
-
     def add_file(self, widget):
-        with Folder(self.base_folder):
+        with Folder(self.base_folder) as folder:
             dialog = gtk.FileChooserDialog(
                 title="Add File",
 
@@ -119,29 +126,37 @@ class WilberUploadDialog(object):
                 buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK)
             )
             dialog.set_select_multiple(True)
+            dialog.set_current_folder(folder)
             response = dialog.run()
         if response == gtk.RESPONSE_OK:
             filenames = dialog.get_filenames()
             for filename in filenames:
                 self.files_model.append([filename])
         dialog.destroy()
+        self.validate_data()
 
     def remove_file(self, widget):
         if len(self.files_model) != 0:
-            (model, iter) = self.selection.get_selected()
-            if iter is not None:
-                model.remove(iter)
-
+            (model, i) = self.selection.get_selected()
+            if i is not None:
+                model.remove(i)
+        self.validate_data()
 
     def get_name(self):
         return self.entry_name.get_text()
 
     def get_description(self):
-        buffer = self.entry_description.get_buffer()
-        start_iter = buffer.get_start_iter()
-        end_iter = buffer.get_end_iter()
-        text = buffer.get_text(start_iter, end_iter, True)
+        entry_buffer = self.entry_description.get_buffer()
+        start_iter = entry_buffer.get_start_iter()
+        end_iter = entry_buffer.get_end_iter()
+        text = entry_buffer.get_text(start_iter, end_iter, True)
         return text
+
+    def get_category(self):
+        label = self.category_dropdown.get_active_text()
+        if label:
+            category = ASSET_TYPE_TO_CATEGORY[label]
+            return category
 
     def get_image(self):
         return self.image_filename
@@ -149,35 +164,32 @@ class WilberUploadDialog(object):
     def get_filenames(self):
         return [row[0] for row in self.files_model]
 
+    def grab_data(self):
+        return {
+            'name': self.get_name(),
+            'description':  self.get_description(),
+            'category':  self.get_category(),
+            'image':  self.get_image(),
+            'filenames':  self.get_filenames(),
+        }
+
     def run(self):
-
         response = self.dialog.run()
-
-        result = {}
-        result['name'] = self.get_name()
-        result['description'] = self.get_description()
-        result['image'] = self.get_image()
-        result['filenames'] = self.get_filenames()
-
+        data = self.grab_data()
         self.dialog.destroy()
+        return response, data
 
-        return response, result
-
-
-    def do_upload(self, data):
-        print('upload')
-
-        name = data['name']
-        description = data['description']
-        image = data['image']
-        filename = data['filenames'][0]
-        category = 'brushes'
-
-        self.api.put_asset(name, category, description, image, filename)
-
-
-    def get_filename(self):
-        return self.dialog.get_filename()
+    def validate_data(self, widget=None, event=None):
+        data = self.grab_data()
+        if not all(data.values()):
+            self.dialog.set_response_sensitive(gtk.RESPONSE_ACCEPT, False)
+            invalid = [k for k, v in data.items() if not v]
+            logger.info('Invalid Data: '+', '.join(invalid))
+            return False
+        else:
+            self.dialog.set_response_sensitive(gtk.RESPONSE_ACCEPT, True)
+            logger.info('Data is valid')
+            return True
 
     def destroy(self, widget, data=None):
         if DEBUG:
